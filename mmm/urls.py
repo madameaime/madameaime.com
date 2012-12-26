b86ff920602943b4e65cf9a4659a0cc410ad31b9
Bertrand Marron <bertrand.marron@gmail.com>
2012-12-26 17:19:15 +0100

    Added favicon.

--- mmm/urls.py
+++ mmm/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, simple
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^favicon\.ico$', simple.redirect_to, { 'url': '/static/img/favicon.ico' }),
+
     url(r'^$',
         TemplateView.as_view(template_name='homepage.html'),
         name='homepage'
