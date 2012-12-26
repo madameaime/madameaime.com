06f6885c681e65a6b13800fc32ea2e52ad02619c
Bertrand Marron <bertrand.marron@gmail.com>
2012-12-26 18:29:00 +0100

    Used STATIC_URL for favicon redirection.

--- mmm/urls.py
+++ mmm/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, simple
 
@@ -5,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^favicon\.ico$', simple.redirect_to, { 'url': '/static/img/favicon.ico' }),
+    url(r'^favicon\.ico$', simple.redirect_to, { 'url': settings.STATIC_URL + 'img/favicon.ico' }),
 
     url(r'^$',
         TemplateView.as_view(template_name='homepage.html'),
