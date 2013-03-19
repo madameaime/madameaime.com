709d9c2e62e4b1096bd488695c15fed9c8924ce4
Julien Castets <castets.j@gmail.com>
2013-03-19 01:55:06 +0100

    Add logistic info on Product model

--- www/stockmgmt/models.py
+++ www/stockmgmt/models.py
@@ -5,16 +5,32 @@ class Supplier(models.Model):
     name = models.CharField(max_length=128)
 
 
+class ProductType(models.Model):
+    shortcode = models.CharField(max_length=16, primary_key=True)
+    description = models.CharField(max_length=64, blank=True)
+
+
 class Product(models.Model):
 
     def __unicode__(self):
         return self.name
 
     name = models.CharField(max_length=255)
-    product_type = models.CharField(max_length=64, blank=True)
+    product_type = models.ForeignKey(ProductType, blank=True,
+                                     null=True, default=None)
+    # cosmetic, book, ...
+    article_family = models.CharField(max_length=64, blank=True)
+    supplier = models.ForeignKey(Supplier, blank=True, null=True, default=None)
     public_price = models.DecimalField(blank=True, null=True, default=None,
                                        max_digits=6, decimal_places=2)
-    supplier = models.ForeignKey(Supplier, blank=True, null=True, default=None)
+    # dimensions info (milimeters)
+    length = models.IntegerField(blank=True, null=True, default=None)
+    width = models.IntegerField(blank=True, null=True, default=None)
+    height = models.IntegerField(blank=True, null=True, default=None)
+    # grams
+    weight = models.IntegerField(blank=True, null=True, default=None)
+    # EAN (ISBN, EAN, GENCOD)
+    models.CharField(max_length=64, blank=True)
 
 
 class Package(Product):
