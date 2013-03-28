a34211d5d992e3d906eab74008c1a62cfc38bbfa
Julien Castets <castets.j@gmail.com>
2013-03-28 15:12:16 +0100

    Return CSV responses for /ads/{products,kits}

--- www/mmm_backoffice/views.py
+++ www/mmm_backoffice/views.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
 
 from braces.views import SuperuserRequiredMixin
@@ -9,6 +12,19 @@ from stockmgmt.models import Product
 from .ads import get_kits_file, get_product_file
 
 
+class CSVResponseMixin(object):
+    response_class = HttpResponse
+
+    def render_to_response(self, context, **response_kwargs):
+        response_kwargs['content_type'] = 'text/csv'
+
+        response = self.response_class(**response_kwargs)
+        writer = csv.writer(response)
+        for line in context['objects']:
+            writer.writerow([value.encode('utf8') for value in line])
+        return response
+
+
 class TransactionsList(SuperuserRequiredMixin, ListView):
     template_name = 'backoffice/be2billtransaction_list.html'
 
@@ -16,19 +32,15 @@ class TransactionsList(SuperuserRequiredMixin, ListView):
         return Be2billTransaction.objects.order_by('-order__date')
 
 
-class ADSProductsView(SuperuserRequiredMixin, TemplateView):
-    template_name = 'backoffice/ads_products.html'
-
+class ADSProductsView(SuperuserRequiredMixin, CSVResponseMixin, TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super(ADSProductsView, self).get_context_data(**kwargs)
-        ctx['products'] = get_product_file(Product.objects.all())
+        ctx['objects'] = get_product_file(Product.objects.all())
         return ctx
 
 
-class ADSKitsView(SuperuserRequiredMixin, TemplateView):
-    template_name = 'backoffice/ads_kits.html'
-
+class ADSKitsView(SuperuserRequiredMixin, CSVResponseMixin, TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super(ADSKitsView, self).get_context_data(**kwargs)
-        ctx['kits'] = get_kits_file(Product.objects.all())
+        ctx['objects'] = get_kits_file(Product.objects.all())
         return ctx
