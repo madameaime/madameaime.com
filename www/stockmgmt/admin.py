448328ef4b259653b87a547706db0bfa9f6920f7
Julien Castets <castets.j@gmail.com>
2013-04-05 17:43:10 +0200

    Improve stockmgmt admin

--- www/stockmgmt/admin.py
+++ www/stockmgmt/admin.py
@@ -3,6 +3,26 @@ from django.contrib import admin
 from models import *
 
 
-admin.site.register(Product)
-admin.site.register(Package)
-admin.site.register(StockMvt)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product_type', 'article_family',
+            'supplier', 'public_price', 'length', 'width', 'height',
+            'weight', 'ean')
+    list_filter = ('product_type',)
+
+admin.site.register(Product, ProductAdmin)
+
+
+class PackageAdmin(admin.ModelAdmin):
+
+    def get_products(package):
+        return ' | '.join([p.name for p in package.products.all()])
+
+    list_display = ('name', get_products, 'product_type')
+
+admin.site.register(Package, PackageAdmin)
+
+
+class StockMvtAdmin(admin.ModelAdmin):
+    list_display = ('date', 'product', 'quantity', 'reason')
+
+admin.site.register(StockMvt, StockMvtAdmin)
